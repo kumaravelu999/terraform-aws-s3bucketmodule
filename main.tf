a2deff66f0022e8326c6181c2b446f4aee8fe211
Kumaravelu <kumaravelu999@gmail.com>
2022-05-31 06:18:43 +0000

    Update main.tf

--- main.tf
+++ main.tf
@@ -4,3 +4,8 @@ resource "aws_s3_bucket" "s3_bucket" {
   bucket = var.bucket_name
     tags = var.tags
 }
+
+resource "aws_s3_bucket_acl" "example_bucket_acl" {
+bucket = aws_s3_bucket.s3_bucket.id
+acl = "private"
+}
